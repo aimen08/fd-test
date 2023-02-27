16166fb17105337f14123ac44a79782157a28096
aimen08 <aimen_hamza@hotmail.com>
2023-02-27 09:58:01 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>FlixFlex</title>
+        <meta name="description" content="Movie App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]"></main>
+    </>
+  );
+};
+
+export default Home;
